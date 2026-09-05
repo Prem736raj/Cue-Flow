bdc2f854a333889d026914dc4f69edf275286ac4
Prem raj <prem736raj@gmail.com>
2026-09-06 02:26:09 +0530

    data: establish safe launch schema baseline

--- app/src/main/java/com/example/data/ScriptDatabase.kt
+++ app/src/main/java/com/example/data/ScriptDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Script::class, Folder::class], version = 3, exportSchema = false)
+@Database(entities = [Script::class, Folder::class], version = 1, exportSchema = true)
 abstract class ScriptDatabase : RoomDatabase() {
     abstract val scriptDao: ScriptDao
 
@@ -15,15 +15,11 @@ abstract class ScriptDatabase : RoomDatabase() {
 
         fun getDatabase(context: Context): ScriptDatabase {
             return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
+                INSTANCE ?: Room.databaseBuilder(
                     context.applicationContext,
                     ScriptDatabase::class.java,
-                    "cueflow_database"
-                )
-                .fallbackToDestructiveMigration()
-                .build()
-                INSTANCE = instance
-                instance
+                    "cueflow_database",
+                ).build().also { INSTANCE = it }
             }
         }
     }
